7888e1586ff58a8451b6509514502945a0d6ccc6
zeeshan <6967538+Zaeche@users.noreply.github.com>
2023-05-27 17:06:27 -0400

    finish basic ui manager
    + handle keyboard + mouse selection at titlescreen

---
+++ Assets/Scripts/UI/UIManager.cs
@@ -0,0 +1,78 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public enum SceneIndices
+{
+    Title = 0,
+    Main = 1
+}
+
+public class UIManager : MonoBehaviour
+{
+    public RectTransform _cursor;
+    public RectTransform _start;
+    public RectTransform _exit;
+    public List<RectTransform> cursorPositions;
+
+    private float m_input = 0f;
+    private const float DELAY = 0.2f;
+    private static UIManager instance = null;
+
+    private int cursorPointer { get; set; } = 0;
+    
+    void Awake()
+    {
+        // the scots object!!!
+        if (UIManager.instance == null) {
+            instance = this;
+        }
+        else {
+            Destroy(this);
+        }
+        // set default cursor position
+        _cursor.anchoredPosition = cursorPositions[cursorPointer].anchoredPosition;
+
+        StartCoroutine("HandleTitleInputs");
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+            if (cursorPointer == 0)
+                StartGame();
+            else
+                ExitGame();
+    }
+
+    private IEnumerator HandleTitleInputs()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(DELAY);
+
+            // when the player taps or holds down, move cursor down to next option
+            // when the player taps or holds up, move cursor up to the next option
+            // when player hits the confirmation button, exit or start game
+            m_input = Input.GetAxis("Vertical");
+
+            if (Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.DownArrow) || m_input < 0f)
+                cursorPointer = cursorPointer < cursorPositions.Count - 1 ? ++cursorPointer : 0;
+
+            if (Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.UpArrow) || m_input > 0f)
+                cursorPointer = cursorPointer > 0 ? --cursorPointer : cursorPositions.Count - 1;
+
+            _cursor.position = cursorPositions[cursorPointer].position;
+        }
+    }
+
+    public void StartGame()
+    {
+        SceneManager.LoadSceneAsync((int)SceneIndices.Main);
+    }
+
+    public void ExitGame() {
+        Application.Quit();
+    }
+}
