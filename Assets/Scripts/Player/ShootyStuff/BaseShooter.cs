5aff7f622d8b17f7c055a6bbcd020a7958427438
Alex Zaranek <xander.zaranek@gmail.com>
2023-05-27 16:44:03 -0400

    Refactored bullet system

--- Assets/Scripts/Player/ShootyStuff/BaseShooter.cs
+++ Assets/Scripts/Player/ShootyStuff/BaseShooter.cs
@@ -2,47 +2,27 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BaseShooter : MonoBehaviour
+public abstract class BaseShooter : MonoBehaviour
 {
-    [Header("Bullet")]
     [SerializeField]
-    GameObject m_BulletPrefab;
-    [SerializeField]
-    Transform m_BulletSpawner;
-    [SerializeField]
-    int m_PoolSize = 3;
-    [SerializeField]
-    float m_BulletCooldown = 0.5f;
-    float m_BulletCooldownTracker = 0;
+    float m_Cooldown = 0.5f;
+    float m_CooldownTracker = 0;
 
-    public bool IsOnCooldown => m_BulletCooldownTracker > 0;
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        for (int i = 0; i < m_PoolSize; i++)
-        {
-            GameObject.Instantiate(m_BulletPrefab, m_BulletSpawner);
-        }
-    }
+    public bool IsOnCooldown => m_CooldownTracker > 0;
 
     // Update is called once per frame
     void Update()
     {
-        if (m_BulletCooldownTracker > 0)
-            m_BulletCooldownTracker -= Time.deltaTime;
+        if (m_CooldownTracker > 0)
+            m_CooldownTracker -= Time.deltaTime;
     }
 
     public void ShootBullet()
     {
-        if (m_BulletSpawner.childCount == 0)
-        {
-            GameObject.Instantiate(m_BulletPrefab, m_BulletSpawner);
-        }
+        OnShootBullet();
 
-        m_BulletSpawner.GetChild(0).GetComponent<BaseBullet>().Activate(m_BulletSpawner);
-        m_BulletCooldownTracker = m_BulletCooldown;
-        //m_Animator.SetTrigger("Fireball");
-        //FireArrowSound.Play();
+        m_CooldownTracker = m_Cooldown;
     }
+
+    protected abstract void OnShootBullet();
 }
