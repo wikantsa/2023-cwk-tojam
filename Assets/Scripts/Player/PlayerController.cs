468cd6e3daf1051ef831e434113f5a6c28d8ada7
Sean Wikant <snwikant@gmail.com>
2023-05-27 14:19:56 -0400

    Changed player prefab

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -10,6 +10,7 @@ public class PlayerController : MonoBehaviour
     public float JumpDuration = 3f;
     public float GroundDistance = 0.2f;
     public float DashDistance = 5f;
+    public float DashForce = 1f;
     public float DashDuration = 1f;
     public float DashCooldown = 4f;
     public float MaxVelocity = 1f;
@@ -19,6 +20,7 @@ public class PlayerController : MonoBehaviour
     
     private bool m_Jumping = false;
     private bool m_Dashing = false;
+    private float m_DashTimer = 0f;
     private float m_DashCooldown = 0f;
 
     private Sequence m_dashSequence;
@@ -26,6 +28,7 @@ public class PlayerController : MonoBehaviour
 
     private Transform m_body;
     private Rigidbody m_Rigidbody;
+    private Vector3 m_movementVector;
 
     void Start()
     {
@@ -35,32 +38,34 @@ public class PlayerController : MonoBehaviour
 
     void Update()
     {
+        
         m_Inputs = Vector3.zero;
         m_Inputs.x = Input.GetAxis("Horizontal");
         m_Inputs.z = Input.GetAxis("Vertical");
 
-        if (m_Inputs != Vector3.zero)
-        {
-            transform.forward = m_Inputs;
-        }
-
         if (Input.GetKeyDown(KeyCode.E) && m_DashCooldown <= 0)
         {
+            //m_Rigidbody.AddForce(m_Inputs * DashForce);
+            /*
             m_Dashing = true;
             m_DashCooldown = DashCooldown;
             m_jumpSequence.TogglePause();
             m_dashSequence = DOTween.Sequence();
-            m_dashSequence.Append(transform.DOBlendableMoveBy(DashDistance * transform.forward, DashDuration).SetEase(Ease.InOutQuad));
+            m_dashSequence.Append(transform.DOBlendableMoveBy(DashDistance * m_Inputs, DashDuration).SetEase(Ease.InOutQuad));
             m_dashSequence.OnComplete(() => { m_Dashing = false; m_jumpSequence.TogglePause();});
+            */
         }
             
         if (Input.GetKeyDown(KeyCode.Space) && !m_Jumping)
         {
+            
+            /*
             m_Jumping = true;
             m_jumpSequence = DOTween.Sequence();
-            m_jumpSequence.Append(m_body.DOBlendableLocalMoveBy(JumpHeight * Vector3.up, JumpDuration/2).SetEase(Ease.OutQuad));
-            m_jumpSequence.Append(m_body.DOBlendableLocalMoveBy(JumpHeight * -Vector3.up, JumpDuration/2).SetEase(Ease.InQuad));
+            m_jumpSequence.Append(m_body.GetChild(0).DOBlendableLocalMoveBy(JumpHeight * Vector3.up, JumpDuration/2).SetEase(Ease.OutQuad));
+            m_jumpSequence.Append(m_body.GetChild(0).DOBlendableLocalMoveBy(JumpHeight * -Vector3.up, JumpDuration/2).SetEase(Ease.InQuad));
             m_jumpSequence.OnComplete(() => { m_Jumping = false; });
+            */
         }
     }
 
@@ -70,8 +75,9 @@ public class PlayerController : MonoBehaviour
         {
             m_DashCooldown -= Time.fixedDeltaTime;
         }
+     
+        Debug.Log(m_Inputs);
 
-        //transform.position = transform.position + m_Inputs * Speed * Time.fixedDeltaTime;
         m_Rigidbody.AddForce(m_Inputs * Speed * Time.fixedDeltaTime);
 
         Vector3 velocity = m_Rigidbody.velocity;
