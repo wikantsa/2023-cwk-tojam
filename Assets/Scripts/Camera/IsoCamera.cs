bdecb708c2e8045ed116d2508bde64d74ddf1ccb
Sean Wikant <snwikant@gmail.com>
2023-05-27 16:52:29 -0400

    Added better camera control

--- Assets/Scripts/Camera/IsoCamera.cs
+++ Assets/Scripts/Camera/IsoCamera.cs
@@ -1,11 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
 
 public class IsoCamera : MonoBehaviour
 {
     public Transform target;
     public Vector3 offset;
+    public float CatchUpSpeed = 1f;
+
+    public float ShakeDuration = 1f;
+    public float ShakeStrength = 1f;
+    public int ShakeVibrato = 1;
+    public float ShakeRandomness = 1f;
 
     // Start is called before the first frame update
     void Start()
@@ -14,8 +21,16 @@ public class IsoCamera : MonoBehaviour
     }
 
     // Update is called once per frame
-    void LateUpdate()
+    void Update()
     {
-        transform.position = target.position + offset;
+        Vector3 destination = target.position + offset;
+        Vector3 location = transform.position;
+        transform.position = transform.position + (destination - location) * Time.deltaTime * CatchUpSpeed;
+
+        if(Input.GetKeyDown(KeyCode.Q))
+        {
+            transform.DOShakePosition(ShakeDuration, ShakeStrength, ShakeVibrato, ShakeRandomness, true, true, ShakeRandomnessMode.Harmonic);
+
+        }
     }
 }
